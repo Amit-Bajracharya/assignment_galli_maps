2d6eb278c3af18b28a1543394319a99e19d50b22
Amit Muni Bajracharya <amitbajracharya4444@gmail.com>
2026-04-23 09:57:04 +0545

    Update app_routes.dart

--- lib/core/router/app_routes.dart
+++ lib/core/router/app_routes.dart
@@ -26,8 +26,7 @@ final appRouter = GoRouter(
             ),
           ],
         ),
-        
-        // BRANCH 1: SAVED (formerly Bookmarks)
+       
         StatefulShellBranch(
           routes: [
             GoRoute(
